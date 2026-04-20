7c15b0b73071427621bea7908493e17068f992dc
Demeter Sándor <sandordemeter27@gmail.com>
2026-04-20 09:54:11 +0200

    controller: DriverController + TeamController mappek hozzáadva

--- src/main/java/com/springapp/F1manager/Controller/DriverController.java
+++ src/main/java/com/springapp/F1manager/Controller/DriverController.java
@@ -1,4 +1,32 @@
 package com.springapp.F1manager.Controller;
 
+
+import com.springapp.F1manager.Model.Driver;
+import com.springapp.F1manager.Service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drivers")
 public class DriverController {
+
+    @Autowired
+    private DriverService driverService;
+
+    @GetMapping
+    public List<Driver> getAllDrivers() {
+        return driverService.getAllDrivers();
+    }
+
+    @PostMapping
+    public Driver createDriver(@RequestBody Driver driver) {
+        return driverService.saveDriver(driver);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDriver(@PathVariable Long id) {
+        driverService.deleteDriver(id);
+    }
 }
--- src/main/java/com/springapp/F1manager/Controller/TeamController.java
+++ src/main/java/com/springapp/F1manager/Controller/TeamController.java
@@ -1,4 +1,31 @@
 package com.springapp.F1manager.Controller;
 
+import com.springapp.F1manager.Model.Team;
+import com.springapp.F1manager.Service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teams")
 public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping
+    public List<Team> getAllTeams() {
+        return teamService.getAllTeams();
+    }
+
+    @PostMapping
+    public Team createTeam(@RequestBody Team team) {
+        return teamService.saveTeam(team);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTeam(@PathVariable Long id) {
+        teamService.deleteTeam(id);
+    }
 }

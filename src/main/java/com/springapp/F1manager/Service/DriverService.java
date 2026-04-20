e985b8fd5e04e6ff7b992d9a5a4f1818ba4058c1
Demeter Sándor <sandordemeter27@gmail.com>
2026-04-20 09:51:04 +0200

    service: CRUD műveletek hozzáadva a DriverService és TeamService osztályokhoz

--- src/main/java/com/springapp/F1manager/Service/DriverService.java
+++ src/main/java/com/springapp/F1manager/Service/DriverService.java
@@ -1,4 +1,27 @@
 package com.springapp.F1manager.Service;
 
+
+import com.springapp.F1manager.Model.Driver;
+import com.springapp.F1manager.Repo.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class DriverService {
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+    public List<Driver> getAllDrivers() {
+        return driverRepository.findAll();
+    }
+
+    public Driver saveDriver(Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    public void deleteDriver(Long id) {
+        driverRepository.deleteById(id);
+    }
 }
--- src/main/java/com/springapp/F1manager/Service/TeamService.java
+++ src/main/java/com/springapp/F1manager/Service/TeamService.java
@@ -1,4 +1,27 @@
 package com.springapp.F1manager.Service;
 
+
+import com.springapp.F1manager.Model.Team;
+import com.springapp.F1manager.Repo.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class TeamService {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+
+    public Team saveTeam(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public void deleteTeam(Long id) {
+        teamRepository.deleteById(id);
+    }
 }

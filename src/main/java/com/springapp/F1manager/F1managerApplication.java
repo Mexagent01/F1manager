93eefb763302b70218f1ae2f208ffd52a7475e11
Demeter Sándor <sandordemeter27@gmail.com>
2026-04-20 08:53:41 +0200

    UPDATE: Spring intilazr-el a projekt létrehozva

---
+++ src/main/java/com/springapp/F1manager/F1managerApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.F1manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1managerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1managerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/F1manager/F1managerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.F1manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1managerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

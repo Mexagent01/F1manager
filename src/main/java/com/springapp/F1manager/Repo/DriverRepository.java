6a3dd65f2206fe7f663c0e88318687ee1c72bc45
Demeter Sándor <sandordemeter27@gmail.com>
2026-04-20 09:47:13 +0200

    repo: JpaRepository interfészek létrehozása a csapatokhoz és versenyzőkhöz

--- src/main/java/com/springapp/F1manager/Repo/DriverRepository.java
+++ src/main/java/com/springapp/F1manager/Repo/DriverRepository.java
@@ -1,4 +1,10 @@
 package com.springapp.F1manager.Repo;
 
-public class DriverRepository {
+
+import com.springapp.F1manager.Model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
 }
--- src/main/java/com/springapp/F1manager/Repo/TeamRepository.java
+++ src/main/java/com/springapp/F1manager/Repo/TeamRepository.java
@@ -1,4 +1,10 @@
 package com.springapp.F1manager.Repo;
 
-public class TeamRepository {
+
+import com.springapp.F1manager.Model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }

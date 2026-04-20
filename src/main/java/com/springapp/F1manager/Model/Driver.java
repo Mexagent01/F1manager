d8cc6a968b416b063fae284845026603343895bd
Demeter Sándor <sandordemeter27@gmail.com>
2026-04-20 09:44:31 +0200

    model: Team és Driver entitások létrehozása ManyToOne kapcsolattal

--- src/main/java/com/springapp/F1manager/Model/Driver.java
+++ src/main/java/com/springapp/F1manager/Model/Driver.java
@@ -1,4 +1,28 @@
 package com.springapp.F1manager.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "drivers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Driver {
+    //Elsődlges kulcs
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private int carNumber;
+    private String nationality;
+    private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team; // Ez köti össze a két táblát
 }
--- src/main/java/com/springapp/F1manager/Model/Team.java
+++ src/main/java/com/springapp/F1manager/Model/Team.java
@@ -1,4 +1,27 @@
 package com.springapp.F1manager.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team {
+    //Elsődleges kulcs
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //A csapat tábla adatai
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String country;
+    private Integer foundedYear;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Driver> drivers;
 }
